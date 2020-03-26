f289aae8622f030a38f95ce59c0b60d8a9326f99
Ivan Martin Alonso <ivan.martin.alonso.89@gmail.com>
2020-03-26 17:35:36 +0100

    First commit empty project

---
+++ src/main/java/com/example/demo/FirstConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstConsumerApplication.class, args);
+    }
+
+}
