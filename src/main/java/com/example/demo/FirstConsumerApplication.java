db4bf5502b2b8f5b6500d177800f4d030fc129ff
Ivan Martin Alonso <ivan.martin.alonso.89@gmail.com>
2020-03-27 12:20:43 +0100

    Adding first consumer with KafkaConsumer

--- src/main/java/com/example/demo/FirstConsumerApplication.java
+++ src/main/java/com/example/demo/FirstConsumerApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import consumer.Consumer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.MessageFormat;
+import java.util.Calendar;
 
 @SpringBootApplication
 public class FirstConsumerApplication {
 
+    private static final Log log = LogFactory.getLog(FirstConsumerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(FirstConsumerApplication.class, args);
+        long startTime = Calendar.getInstance().getTimeInMillis();
+
+        long endTime = Calendar.getInstance().getTimeInMillis();
+        double startingAppTimeInSeconds = (endTime - startTime) / 1000.0;
+
+        log.info("--------------------------------------------------------------------");
+        log.info(
+                MessageFormat.format(
+                        "|||| Consumer 1 - Ivan Martin - PFG -  Application started successfully in {0} seconds ||||",
+                        startingAppTimeInSeconds));
+        log.info("--------------------------------------------------------------------");
+        Consumer tienda = new Consumer();
+        tienda.createKafkaConsumer();
     }
 
 }
---
+++ src/main/java/consumer/Consumer.java
@@ -0,0 +1,66 @@
+package consumer;
+
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+
+
+public class Consumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Consumer.class);
+
+    private KafkaConsumer<String, String> kafkaConsumerClient;
+
+    public void createKafkaConsumer() {
+
+        // Configure the properties and create the Kafka consumer
+        Properties props = new Properties();
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "Tienda-1-Consumer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "New stock");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+
+        // Initialize the kafka consumer instance
+        kafkaConsumerClient = new KafkaConsumer<>(props);
+        kafkaConsumerClient.subscribe(Arrays.asList("test_1"));
+
+        checkNewStockAvailability();
+
+
+
+    }
+
+    public void checkNewStockAvailability() {
+        ConsumerRecords<String, String> records;
+        if (kafkaConsumerClient == null) {
+            LOG.warn("Kafka consumer will not be initialized!");
+        }
+        LOG.info("Lets poll kafka topic");
+        while (true) {
+            records = kafkaConsumerClient.poll(Duration.of(1000, ChronoUnit.MILLIS));
+            for (ConsumerRecord<String, String> record : records) {
+                if (record != null && record.value() != null) {
+                    final String event = record.value();
+                    LOG.info("Consuming event: {}", event);
+                    LOG.info("event: {}", event);
+
+                }
+            }
+        }
+    }
+
+}
