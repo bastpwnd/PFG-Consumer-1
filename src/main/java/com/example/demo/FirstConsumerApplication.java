bec497eb3c2d6b4a4abd87e0a7716195b7c194a5
Ivan Martin Alonso <ivan.martin.alonso.89@gmail.com>
2020-05-08 17:25:45 +0200

    Adding Kafka listener and scheduled to print products

--- src/main/java/com/example/demo/FirstConsumerApplication.java
+++ src/main/java/com/example/demo/FirstConsumerApplication.java
@@ -5,15 +5,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.text.MessageFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "consumer" })
+@EnableScheduling
 public class FirstConsumerApplication {
 
     private static final Log log = LogFactory.getLog(FirstConsumerApplication.class);
-
+    public static Map<String,Integer> productos = new HashMap<>();;
     public static void main(String[] args) {
         SpringApplication.run(FirstConsumerApplication.class, args);
         long startTime = Calendar.getInstance().getTimeInMillis();
@@ -27,8 +32,9 @@ public class FirstConsumerApplication {
                         "|||| Consumer 1 - Ivan Martin - PFG -  Application started successfully in {0} seconds ||||",
                         startingAppTimeInSeconds));
         log.info("--------------------------------------------------------------------");
-        Consumer tienda = new Consumer();
-        tienda.createKafkaConsumer();
+        //Activar esta opcion si no se quiere usar el la escucha mediante un listener*/
+        /*Consumer tienda = new Consumer();
+        tienda.createKafkaConsumer();*/
     }
 
 }
---
+++ src/main/java/consumer/ConsumerWithListener.java
@@ -0,0 +1,38 @@
+package consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ConsumerWithListener {
+
+    private Logger LOG = LoggerFactory.getLogger(ConsumerWithListener.class.getName());
+    private static Map<Integer, String> products = new HashMap<>();
+
+    @KafkaListener(topics = "test_1", groupId = "Tienda-1")
+    public void receive(ConsumerRecord<String, String> record){
+
+        LOG.info("Consuming event: {}", record.value());
+        insertDataInMap(record.value());
+    }
+
+    public void insertDataInMap(String evento){
+
+        String data []= evento.split(":");
+        products.put(Integer.parseInt(data[1]),data[0]);
+    }
+    @Scheduled(cron = "*/10 * * * * ?")
+    public void printProductsById(){
+        LOG.info("Imprimiendo productos recibidos en la tienda:");
+        products.forEach((k,v)->System.out.println("Tipo de producto : " + v + " ID : " + k));
+    }
+
+
+
+}
